0609b6a1a571b0ee02a810f73cdb9ccdc8779f9f
Arilson Lima <arilson@kali>
2026-04-22 10:28:53 -0300

    Update server.js to bind to 0.0.0.0 and explicit dirname

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({ dev, dir: __dirname })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(process.env.PORT || 3000, (err) => {
+  }).listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`)
+    console.log(`> Ready on http://0.0.0.0:${process.env.PORT || 3000}`)
   })
 })
